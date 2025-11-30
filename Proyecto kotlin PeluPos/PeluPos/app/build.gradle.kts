55d4d04b772d0284118fa8daffb696e53985ac69
DavidRafaelGdaniec <davgda@alu.edu.gva.es>
2025-11-30 20:29:54 +0100

    Se añaden los colores definidos anteriormente

--- Proyecto kotlin PeluPos/PeluPos/app/build.gradle.kts
+++ Proyecto kotlin PeluPos/PeluPos/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.pmdm.PeluPos"
+        applicationId = "com.proyecto.PeluPos"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
@@ -64,21 +64,20 @@ dependencies {
     implementation(libs.pmdm.ies.balmis.components)
     implementation(libs.pmdm.ies.balmis.utilities)
 
-//    implementation(libs.dagger.hilt.android)
-//    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
 //    ksp(libs.dagger.hilt.android.compiler)
 //    kspAndroidTest(libs.dagger.hilt.android.compiler)
-//
-//    implementation(libs.compose.navigation)
-//
-//    implementation(libs.androidx.room.ktx)
+
+    implementation(libs.compose.navigation)
+
+    implementation(libs.androidx.room.ktx)
 //    ksp(libs.androidx.room.compiler)
-//
-//    implementation(libs.com.squareup.retrofit2.converter.gson)
-//    implementation(libs.com.squareup.retrofit2.retrofit)
-//    implementation(platform(libs.com.squareup.okhttp3.okhttp.bom))
-//    implementation(libs.com.squareup.okhttp3.okhttp)
-//    implementation(libs.com.squareup.okhttp3.logging.interceptor)
+    implementation(libs.com.squareup.retrofit2.converter.gson)
+    implementation(libs.com.squareup.retrofit2.retrofit)
+    implementation(platform(libs.com.squareup.okhttp3.okhttp.bom))
+    implementation(libs.com.squareup.okhttp3.okhttp)
+    implementation(libs.com.squareup.okhttp3.logging.interceptor)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
