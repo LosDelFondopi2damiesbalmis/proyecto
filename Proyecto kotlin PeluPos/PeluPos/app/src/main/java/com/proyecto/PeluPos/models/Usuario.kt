12c4bb0982fcc311f2f786ff6208f62aaa1aa1b7
DavidRafaelGdaniec <davgda@alu.edu.gva.es>
2026-01-12 08:17:24 +0100

    Se añade enum de roles para usuario

--- Proyecto kotlin PeluPos/PeluPos/app/src/main/java/com/proyecto/PeluPos/models/Usuario.kt
+++ Proyecto kotlin PeluPos/PeluPos/app/src/main/java/com/proyecto/PeluPos/models/Usuario.kt
@@ -1,7 +1,13 @@
 package com.proyecto.PeluPos.models
+enum class RolUsuario {
+    ADMINISTRADOR,
+    MANAGER,
+    EMPLEADO
+}
 data class Usuario(
     val idUsuario: Long,
     var usuario: String,
     var contrasena: String,
-    val empleado: Empleado
+    val empleado: Empleado,
+    val rolUsuario: RolUsuario
 )
