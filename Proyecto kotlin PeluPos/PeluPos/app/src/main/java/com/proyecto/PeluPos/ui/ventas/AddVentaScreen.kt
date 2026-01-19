a8f1e016c7f321f9b229d1c875a3f8579a81655e
DavidRafaelGdaniec <davgda@alu.edu.gva.es>
2026-01-19 09:50:56 +0100

    movido ventas a ui

--- Proyecto kotlin PeluPos/PeluPos/app/src/main/java/com/proyecto/PeluPos/ui/ventas/AddVentaScreen.kt
+++ Proyecto kotlin PeluPos/PeluPos/app/src/main/java/com/proyecto/PeluPos/ui/ventas/AddVentaScreen.kt
@@ -1,9 +1,7 @@
-package com.proyecto.PeluPos.ventas
+package com.proyecto.PeluPos.ui.ventas
 
-import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,8 +13,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -205,7 +201,7 @@ fun PaymentOptionButton(
         modifier = modifier.height(80.dp),
         shape = RoundedCornerShape(12.dp),
         color = backgroundColor,
-        border = androidx.compose.foundation.BorderStroke(if (isSelected) 2.dp else 1.dp, borderColor)
+        border = BorderStroke(if (isSelected) 2.dp else 1.dp, borderColor)
     ) {
         Column(
             verticalArrangement = Arrangement.Center,
